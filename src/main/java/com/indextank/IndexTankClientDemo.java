be0f46718fe2f1092ea3b307fe92fa0d7a657a3d
Adrián <adrian@indextank.com>
2011-03-31 23:22:34 -0300

    List indexes demo

--- src/main/java/com/indextank/IndexTankClientDemo.java
+++ src/main/java/com/indextank/IndexTankClientDemo.java
@@ -1,5 +1,18 @@
 package com.indextank;
 
+import java.io.IOException;
+
+import com.flaptor.indextank.apiclient.IndexDoesNotExistException;
+import com.flaptor.indextank.apiclient.IndexTankClient;
+import com.flaptor.indextank.apiclient.IndexTankClient.Index;
+
 public class IndexTankClientDemo {
+  
+  public static void main(String[] args) throws IOException, IndexDoesNotExistException {
+    IndexTankClient client = new IndexTankClient("<public-api>","<auth-info>");
+    for(Index index: client.listIndexes()) {
+      System.out.println(index.getCode());
+    }
+  }
 
 }
